f2cdcb6158bbddade528442d1eebce71f7e11e0f
David Marinović <marinovic.business@gmail.com>
2023-09-27 12:55:44 +0200

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -60,7 +60,7 @@ function App() {
 
             if (profileObj) {
                 const response = await fetch(
-                    "http://localhost:8080/api/v1/users",
+                    "https://refine-dashboard-wui2.onrender.com/0/api/v1/users",
                     {
                         method: "POST",
                         headers: { "Content-Type": "application/json" },
@@ -131,7 +131,7 @@ function App() {
             <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
             <RefineSnackbarProvider>
                 <Refine
-                    dataProvider={dataProvider("http://localhost:8080/api/v1")}
+                    dataProvider={dataProvider("https://refine-dashboard-wui2.onrender.com/api/v1")}
                     notificationProvider={notificationProvider}
                     ReadyPage={ReadyPage}
                     catchAll={<ErrorComponent />}
